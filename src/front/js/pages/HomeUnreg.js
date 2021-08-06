63589dfaac38f54bf6e24a5634aad509e5c20c50
Antoniya <anemtserova@gmail.com>
2021-08-06 09:28:56 +0000

    finished the landing page

--- src/front/js/pages/HomeUnreg.js
+++ src/front/js/pages/HomeUnreg.js
@@ -16,37 +16,63 @@ export const HomeUnreg = () => {
 			{/* <Grid /> */}
 
 			<>
-				<h1 className="text-center py-3 heading mb-1">Start Designing Your Garden</h1>
+				<h1 className="text-center py-3 heading mb-1">Welcome to Plotters!</h1>
 				<p className="text-center m-0">
 					<img src="https://i.imgur.com/tTBj7Rj.png" alt="floral-design" />
 				</p>
-				<div className="row">
-					<div className="col-7">
+				<div className="row row-cols-2 text-center">
+					<div className="col m-auto">
 						<div className="landing-titles text-center d-flex justify-content-center align-items-center mt-5">
 							<h2 className="m-0">What is Plotters?</h2>
 						</div>
 						<p className="landing-text mx-5 mb-5 p-3">
-							Whether you are a beginner or an experienced gardener, you can always use{" "}
+							Whether you are a beginner or an experienced gardener, you can always take advantage of{" "}
 							<b className="color-item">Plotters</b> to plan your dream garden! It is easy and fast!
 						</p>
 					</div>
-					<div className="col-5 mt-5">Img HERE</div>
+					<div className="col m-auto ml-5 landing-img">
+						<img className="m-auto" src="https://i.imgur.com/4ibgmga.png" alt="flowers" />
+					</div>
+					<div className="w-50 m-auto">
+						<img className="m-auto" src="https://i.imgur.com/KZ6RiAX.png" alt="..." />
+					</div>
 				</div>
-				<div className="row">
-					<div className="col-5">Img HERE</div>
-					<div className="col-7">
+				<div className="row row-cols-2 m-5 text-center">
+					<div className="col landing-img m-auto">
+						<img className="m-auto" src="https://i.imgur.com/zLqI9Ec.png" alt="flowers" />
+					</div>
+					<div className="col my-3 d-flex flex-column justify-content-center">
 						<div className="landing-titles text-center d-flex justify-content-center align-items-center mt-5">
 							<h2>How does Plotters work?</h2>
 						</div>
-						<span className="landing-numbers">1.</span>
-						<p className="landing-text mx-5 mb-5 p-3">Sign up quickly</p>
-						<span className="landing-numbers">2.</span>
-						<p className="landing-text mx-5 mb-5 p-3">Enter a few details about your spot</p>
-						<span className="landing-numbers">3.</span>
-						<p className="landing-text mx-5 mb-5 p-3">Explore plant choices and create your garden.</p>
+						<div className="m-auto">
+							<div className="d-flex align-items-center my-1 ml-5 pl-5">
+								<span className="landing-numbers mr-3">
+									<img src="https://i.imgur.com/NCT1ojK.png" alt="1" />
+								</span>
+								<p className="landing-text mb-0 p-3">Click on the grid and sign up.</p>
+							</div>
+							<div className="d-flex align-items-center my-1 ">
+								<span className="landing-numbers mr-3">
+									<img src="https://i.imgur.com/s58ycCz.png" alt="2" />
+								</span>
+								<p className="landing-text mb-0 p-3">Tell us about your garden spot.</p>
+							</div>
+							<div className="d-flex align-items-center my-1 ml-4 pl-3">
+								<span className="landing-numbers mr-3">
+									<img src="https://i.imgur.com/BueJMz8.png" alt="3" />
+								</span>
+								<p className="landing-text mb-0 p-3">Create your dream garden.</p>
+							</div>
+						</div>
+					</div>
+					<div className="w-50 m-auto">
+						<img className="m-auto" src="https://i.imgur.com/KZ6RiAX.png" alt="..." />
 					</div>
 				</div>
-				<div className="container justify-content-center d-flex flex-row flex-wrap my-2 py-5">
+				<h1 className="text-center py-3 heading mb-1">Ready to begin?</h1>
+				<h1 className="text-center pb-3 heading mb-1">Start Designing Your Garden</h1>
+				<div className="container justify-content-center d-flex flex-row flex-wrap my-2 pt-3">
 					{store.hardcodedGarden.map((el, i) => (
 						<div className="square zoom-in" style={{ width: width, height: width, margin: margin }} key={i}>
 							<Link to="/signuppersonal">
@@ -57,9 +83,9 @@ export const HomeUnreg = () => {
 				</div>
 			</>
 
-			<div className="container d-flex flex-column align-items-center">
-				<Link to="/signuppersonal">
-					<button type="button" className="btn btn-style btn-list m-3">
+			<div className="container d-flex flex-column align-items-center w-50">
+				<Link to="/signuppersonal" className="w-50">
+					<button type="button" className=" w-100 btn btn-style btn-list mx-3 mb-3">
 						Sign up
 					</button>
 				</Link>
